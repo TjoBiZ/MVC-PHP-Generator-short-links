c0eb7b5c4750c0d497181241927b49dc1c658531
joker <joker@tjo.biz>
2020-04-09 04:35:56 +0800

    PHP MVC Framework with MySQL

--- config/routes.php
+++ config/routes.php
@@ -3,9 +3,6 @@
 	use tjo\Router;
 	use tjo\CheckSLinks;
 
-	//This rule check short links in base for then redirection real link (Logic 'then' in the controller)
-	Router::add('^[A-Za-z0-9]{6}', CheckSLinks::checkShortLinksInBase());
-
 	//default routes
 	//This rules for admin panel
 	Router::add('^admin$', ['controller' => 'Main', 'action' => 'index', 'prefix' => 'admin']);
@@ -14,3 +11,6 @@
 	//This rules for users page in public
 	Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
 	Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$'); //Take url and trim regex controller/action https://site.com/controller/action
+
+	//This rule check short links in base for then redirection real link (Logic 'then' in the controller)
+	Router::add('^[A-Za-z0-9]{6}', CheckSLinks::checkShortLinksInBase());
