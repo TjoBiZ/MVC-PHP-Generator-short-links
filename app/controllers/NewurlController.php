9ff4578d85955d7047702ba534f9fc1d21e196c6
joker <joker@tjo.biz>
2020-04-09 13:49:01 +0800

    PHP MVC Framework with MySQL

--- app/controllers/NewurlController.php
+++ app/controllers/NewurlController.php
@@ -9,6 +9,22 @@ class NewurlController extends AppController {
 			//debug($this->view);
 			//debug($this->controller );
 			//echo __METHOD__;
+			//Regex inversion - example (?<=r)find - this find word "find" before letter 'r'.
+			preg_match('/(?<=:\/\/).*/', $_POST['reallink'], $reallink);
+			if (!isset($reallink[0])) {
+				$reallink[0] = $_POST['reallink'];
+			}
+			$reallink[0] = htmlspecialchars($reallink[0]);
+			//"SELECT * FROM slinks.slinks WHERE shortlink LIKE '%$shortlink%'"
+			$resultlinks = \R::getRow( "SELECT * FROM slinks.slinks WHERE reallink LIKE ? LIMIT 1", [ "%$reallink[0]%" ]);
+
+			if(isset($resultlinks['shortlink'])) {
+				echo json_encode(['reallink' => $resultlinks['reallink'], 'shortlink' => $resultlinks['shortlink']]);
+				die();
+			} else {
+				echo 'generation new link';
+			}
+
 			echo 'This is generator links';
 		}
 
